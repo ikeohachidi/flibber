42ad0ad8a4883926b09a281b34f6150122bd24c9
ikeohachidi <ikeohachidi@gmail.com>
2022-01-08 22:33:21 +0100

    feat: create avatar group component

---
+++ src/components/Avatar/AvatarGroup.tsx
@@ -0,0 +1,13 @@
+import User from "types/User"
+
+type Props = {
+	users: User[]
+}
+
+const AvatarGroup = (props: Props): JSX.Element => {
+	return (
+		<div></div>
+	)
+}
+
+export default AvatarGroup
