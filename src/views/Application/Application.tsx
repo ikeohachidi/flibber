9dabc4e1376e1a27a76ba2296f1672ac677a1eed
ikeohachidi <ikeohachidi@gmail.com>
2022-01-13 22:00:47 +0100

    feat: validate user authentication

--- src/views/Application/Application.tsx
+++ src/views/Application/Application.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 
 import SideNav from 'components/SideNav/SideNav';
 import Chat from 'components/Chat/Chat';
 import Messages from 'components/Messages/Messages';
 
+import { activeUser } from 'services/authentication';
+import RoutePath from 'routes';
+
 const Application = () => {
+	const navigation = useNavigate();
+
+	useEffect(() => {
+		if (activeUser() === null) {
+			navigation(RoutePath.LOGIN);
+		}
+	})
+
 	return (
 		<div className="App grid grid-cols-8">
 			<div className="col-span-2">
