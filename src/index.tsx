6fabf010d08356075954e13a11a8994e50db652e
ikeohachidi <ikeohachidi@gmail.com>
2022-01-15 17:07:18 +0100

    feat: add store to root

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import { BrowserRouter } from "react-router-dom";
 
 import './index.css';
 import 'remixicon/fonts/remixicon.css'
+import store from 'store';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={ store }>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
