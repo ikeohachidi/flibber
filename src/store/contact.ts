4f0f89aa2aa00289e495ae1e049df25ee4901303
ikeohachidi <ikeohachidi@gmail.com>
2022-01-16 21:08:37 +0100

    feat: add store extraReducers for contact request

--- src/store/contact.ts
+++ src/store/contact.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { fetchPendingRequestService } from 'services/contact';
+import { acceptContactRequestService, declineContactRequestService, fetchPendingRequestService } from 'services/contact';
 import User from "types/User";
 
 type Action<T> = PayloadAction<T>
@@ -21,6 +21,20 @@ const initialState = {
 	status: Status.FULFILLED 
 }
 
+type ContactType = 'acceptedContacts' | 'pendingContacts';
+
+const removeFromContacts = (state: ContactsState, contactType: ContactType, user: User) => {
+	const index = state[contactType].findIndex(contact => contact.id === user.id)
+	if (index === -1) return;
+
+	state.pendingContacts.splice(index, 1);
+}
+
+
+const addContact = (state: ContactsState, contactType: ContactType, user: User) => {
+	state[contactType].push(user);
+}
+
 const contact = createSlice({
 	name: 'contact',
 	initialState,
@@ -31,6 +45,12 @@ const contact = createSlice({
 		setPendingContacts(state: ContactsState, action: Action<User[]>) {
 			state.pendingContacts = action.payload;
 		},
+		removePendingContact(state: ContactsState, action: Action<User>) {
+			removeFromContacts(state, 'pendingContacts', action.payload)
+		},
+		addAcceptedContact(state: ContactsState, action: Action<User>) {
+			addContact(state, 'acceptedContacts', action.payload);
+		}
 	},
 	extraReducers: (builder) => {
 		builder
@@ -39,6 +59,19 @@ const contact = createSlice({
 			})
 			.addCase(fetchPendingRequestService.fulfilled, (state: ContactsState, action) => {
 				state.pendingContacts = action.payload as User[];
+			})
+			.addCase(acceptContactRequestService.fulfilled, (state: ContactsState, action) => {
+				if (action.payload) {
+					addContact(state, 'acceptedContacts', action.payload);
+				}
+			})
+			.addCase(declineContactRequestService.pending, (state, action) => {
+				state.status = Status.PENDING;
+			})
+			.addCase(declineContactRequestService.fulfilled, (state: ContactsState, action) => {
+				if (action.payload) {
+					removeFromContacts(state, 'pendingContacts', action.payload);
+				}
 			})
 	}
 })
