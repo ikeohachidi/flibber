1b1084bc5ae957b88d9c1fc338708d6ade733c57
ikeohachidi <ikeohachidi@gmail.com>
2022-04-03 20:45:29 +0100

    feat: create RecentChat type

--- src/types/Chat.ts
+++ src/types/Chat.ts
@@ -1,15 +1,25 @@
+import User from "./User";
+
 export enum ChatType {
 	TEXT = 'text',
 	IMAGE = 'image',
 	VIDEO = 'video'
 }
 
+interface Message {
+	type: ChatType;
+	value: string;
+}
 export default interface Chat {
 	from: number;
 	to: number;
 	created_at?: string;
-	message: {
-		type: ChatType;
-		value: string;
-	}
+	message: Message 
+}
+
+export interface RecentChat {
+	from: User;
+	to: User;
+	created_at: string;
+	message: Message;
 }
