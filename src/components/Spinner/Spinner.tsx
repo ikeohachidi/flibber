d7092e2e39c673f8a25f390c863f74cf697e04fa
ikeohachidi <ikeohachidi@gmail.com>
2022-05-21 21:24:45 +0100

    refactor: make size a prop for spinner component

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -2,20 +2,32 @@ import React from 'react';
 import './Spinner.css';
 
 type Props = {
-	fullSize: boolean;
+	fullSize?: boolean;
+	size?: string;
 }
 
 const Spinner = (props: Props) => {
 	if (props.fullSize) {
 		return (
 			<div className="loader-spinner-wrapper">
-				<div className="loader-spinner"></div>
+				<div 
+					className="loader-spinner"
+					style={{
+						height: props.size,
+						width: props.size
+					}}
+				></div>
 			</div>	
 		)
 	}
 	return (
 		<div className="loader-spinner"></div>
 	)
+}
+
+Spinner.defaultProps = {
+	fullSize: false,
+	size: '40px'
 }
 
 export default Spinner;
