2992ce48a56c155dd3ffc054378880c88a9a2570
ikeohachidi <ikeohachidi@gmail.com>
2022-07-02 15:31:41 -0700

    refactor: componentize one-to-one chat view

--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -1,125 +1,66 @@
-import { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import MessageInput from './MessageInput/MessageInput';
-import Avatar from 'components/Avatar/Avatar';
-import ChatDetails from './ChatDetails/ChatDetails';
+import SingleChat from './SingleChat/SingleChat';
 import Spinner from 'components/Spinner/Spinner';
 
 import './ChatWindow.css';
 
 import { AppState } from 'store';
-import { getConversationService } from 'services/chat';
-import { chatId } from 'utils/chat';
 
-import Chat, { ChatType } from 'types/Chat'
 import User from 'types/User';
 import { Channel } from 'types/Channel';
-import { getChannelMessagesService } from 'services/channel';
-
-/**
- * checks if a single user has sent consecutive messages without another
- * person sending a message
- * @param Message message object
- * @returns boolean
- */
-const isMessageInAStreak = (messageIndex: number, messages: Chat[]) => {
-	if (messages.length === 0 || messageIndex === 0) return false;
-
-	const presentChat = messages[messageIndex];
-	const prevChat  = messages[messageIndex - 1];
-
-	return presentChat.from === prevChat.from;
-}
 
 const ChatWindow = (): JSX.Element => {
 	const dispatch = useDispatch();
-	const isSignedInUserSender = (senderId: number) => senderId === authUser.id;
 
 	const activeChatUser = useSelector<AppState, User | null>(state => state.chat.activeUserChat);
 	const activeChannel = useSelector<AppState, Channel | null>(state => state.channel.activeChannel);
 	const authUser = useSelector<AppState, User>(state => state.user.user!);
-	const conversations = useSelector<AppState, Chat[]>(state => {
-		if (activeChatUser && (activeChatUser.id in state.chat.conversation)) {
-			return state.chat.conversation[activeChatUser.id];
-		}
+	const isFetchingSingleConversation = useSelector<AppState, boolean>(state => state.chat.isFetchingConversation);
 
-		return [];
-	});
-	const isFetchingConversation = useSelector<AppState, boolean>(state => state.chat.isFetchingConversation);
-	const loadedConversations = useSelector<AppState, number[]>(state => state.chat.loadedConversations);
+	const isLoading = (): boolean => {
+		if (isFetchingSingleConversation) return true;
 
-	useEffect(() => {
-		if (!activeChatUser || activeChatUser.id === 0) return;
-
-		if (!loadedConversations.includes(activeChatUser.id)) {
-			dispatch(getConversationService({
-				chatId: chatId(authUser.id, activeChatUser.id),
-				authUser: authUser.id
-			}))
-		}
-
-	}, [ activeChatUser ])
-
-	useEffect(() => {
-		if (!activeChannel || activeChannel.id === 0) return;
-		
-		dispatch(getChannelMessagesService(activeChannel.id as number)) 
-	}, [ activeChannel ])
-	const chatDetailsEl = useRef<HTMLDivElement>(null);
-
-	const onCloseClick = () => {
-		chatDetailsEl.current?.classList.toggle('hide');
+		return false;
 	}
 
-	const title = () => {
-		if (activeChatUser) return activeChatUser.name;
-		
-		if (activeChannel) return activeChannel.name;
+	const chatType = (): 'user' | 'channel' | 'none' => {
+		if (activeChannel && activeChannel.id) {
+			return 'channel';
+		}
 
-		return 'No active chat participant'; }
+		if (activeChatUser && activeChatUser.id) {
+			return 'user';
+		}
+
+		return 'none';
+	}
+
+
+	const ChatTypeDisplay = () => {
+		switch (chatType()) {
+			case 'user':
+				return <SingleChat authUser={ authUser }/>
+			case 'channel':
+				return <h1>Channel</h1>
+			default:
+				return <h1>Neither</h1>
+		}
+	}
 
 	return (
 		<section className="chat-container">
+
 			{
-				isFetchingConversation &&
-				<Spinner 
+				isLoading()
+				? <Spinner 
 					fullSize={ true }
 				/>
+				: <ChatTypeDisplay />
 			}
-
-			<div className="chat-details hide" ref={ chatDetailsEl }>
-				<ChatDetails onCloseClick={ onCloseClick }/>
-			</div>
-			<div className="chat-banner">
-				<span>
-					{ title() }
-				</span>
-				<i className="ri-more-fill" onClick={ onCloseClick }></i>
-			</div>
-
-			<div className="chat-box">
-				{
-					conversations.map((conversation, index) => (
-						<div 
-							className={ `chat ${isSignedInUserSender(conversation.from) ? 'sending' : 'receiving'}` } 
-							key={ index }
-						>
-							<div className="w-1/12">
-								{ isMessageInAStreak(index, conversations) === false 
-									?  <Avatar dimension={ 30 }/>
-									: <span></span> 
-								}
-							</div>
-							<div className="w-11/12 mx-3">
-								{ conversation.message.type === ChatType.TEXT &&
-									<p className="text-bubble">{ conversation.message.value }</p>
-								}
-							</div>
-						</div>
-					))	
-				}
-			</div>
+			
 			<div className="message-input">
 				<MessageInput 
 					activeChatUser={ activeChatUser }
---
+++ src/components/ChatWindow/SingleChat/SingleChat.tsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Avatar from 'components/Avatar/Avatar';
+import ChatDetails from '../ChatDetails/ChatDetails';
+
+import '../ChatWindow.css';
+
+import { AppState } from 'store';
+import { getConversationService } from 'services/chat';
+import { chatId } from 'utils/chat';
+
+import Chat, { ChatType } from 'types/Chat'
+import User from 'types/User';
+
+
+/**
+ * checks if a single user has sent consecutive messages without another
+ * person sending a message
+ * @param Message message object
+ * @returns boolean
+ */
+const isMessageInAStreak = (messageIndex: number, messages: Chat[]) => {
+	if (messages.length === 0 || messageIndex === 0) return false;
+
+	const presentChat = messages[messageIndex];
+	const prevChat  = messages[messageIndex - 1];
+
+	return presentChat.from === prevChat.from;
+}
+
+
+interface Props {
+	authUser: User
+}
+
+const SingleChat = ({ authUser }: Props): JSX.Element => {
+	const dispatch = useDispatch();
+
+	const activeChatUser = useSelector<AppState, User>(state => state.chat.activeUserChat!);
+	const loadedConversations = useSelector<AppState, number[]>(state => state.chat.loadedConversations);
+	const conversations = useSelector<AppState, Chat[]>(state => {
+		if (activeChatUser.id in state.chat.conversation) {
+			return state.chat.conversation[activeChatUser.id];
+		}
+
+		return [];
+	});
+
+	const isSignedInUserSender = (senderId: number) => senderId === authUser.id;
+
+	const chatDetailsEl = useRef<HTMLDivElement>(null);
+	const toggleChatDetails = (state: boolean): void => {
+		if (state) {
+			chatDetailsEl.current?.classList.remove('hide');
+		} else {
+			chatDetailsEl.current?.classList.add('hide');
+		}
+	}
+
+	useEffect(() => {
+		if (!loadedConversations.includes(activeChatUser.id)) {
+			dispatch(getConversationService({
+				chatId: chatId(authUser.id, activeChatUser.id),
+				authUser: authUser.id
+			}))
+		}
+
+	}, [ activeChatUser ])
+
+	return (
+		<>
+			<div className="chat-details hide" ref={ chatDetailsEl }>
+				<ChatDetails onCloseClick={ () => toggleChatDetails(false) }/>
+			</div>
+			<div className="chat-banner">
+				<span>
+					{ activeChatUser.name }
+				</span>
+				<i className="ri-more-fill" onClick={ () => toggleChatDetails(true) }></i>
+			</div>
+
+			<div className="chat-box">
+				{
+					conversations.map((conversation, index) => (
+						<div 
+							className={ `chat ${isSignedInUserSender(conversation.from) ? 'sending' : 'receiving'}` } 
+							key={ index }
+						>
+							<div className="w-1/12">
+								{ isMessageInAStreak(index, conversations) === false 
+									?  <Avatar dimension={ 30 }/>
+									: <span></span> 
+								}
+							</div>
+							<div className="w-11/12 mx-3">
+								{ conversation.message.type === ChatType.TEXT &&
+									<p className="text-bubble">{ conversation.message.value }</p>
+								}
+							</div>
+						</div>
+					))	
+				}
+			</div>
+		</> 
+	)
+}
+
+export default SingleChat;
