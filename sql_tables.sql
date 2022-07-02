0f05b6997fa179810a2759ad31da0f1c0fdbbcee
ikeohachidi <ikeohachidi@gmail.com>
2022-07-02 12:35:17 -0700

    docs: update table creation sql file

--- sql_tables.sql
+++ sql_tables.sql
@@ -1,35 +1,24 @@
-## Tables that need to be created to successfully run your own instance of Flibber
+-- Tables that need to be created to successfully run your own instance of Flibber
 
-##### User 
-```sql
 CREATE TABLE "user" (
 	id 					bigserial PRIMARY KEY NOT NULL,
 	name 				text NOT NULL,
 	onlineState 		text NOT NULL DEFAULT 'offline',
 	email 				text NOT NULL
 );
-```
 
-##### Contact
-```sql
 CREATE TABLE contact (
 	id 					bigserial PRIMARY KEY NOT NULL,
 	user1				bigint REFERENCES "user"(id) NOT NULL ON DELETE CASCADE,
 	user2				bigint REFERENCES "user"(id) NOT NULL on DELETE CASCADE
 );
-```
 
-##### Contact request
-```sql
 CREATE TABLE contact_request (
 	id 					bigserial PRIMARY KEY NOT NULL,
 	requestee			bigint REFERENCES "user"(id) NOT NULL ON DELETE CASCADE,
 	requester			bigint REFERENCES "user"(id) NOT NULL ON DELETE CASCADE
 );
-```
 
-##### Chat
-```sql
 CREATE TABLE chat (
 	id 					bigserial PRIMARY KEY NOT NULL,
 	created_at 			timestamp NOT NULL DEFAULT now(),
@@ -38,4 +27,3 @@ CREATE TABLE chat (
 	message 			json NOT NULL,
 	conversation_id 	bigint NOT NULL
 );
-```
